24b3e89b1b1fdb4645460a19e7e088fc21d50bb0
Igor <196232533+BGNS42@users.noreply.github.com>
2026-04-08 17:27:11 -0300

    Adicionado headerHome

--- AcademyFirstChallenge/HomePage.swift
+++ AcademyFirstChallenge/HomePage.swift
@@ -10,17 +10,50 @@ import SwiftUI
 struct HomePage: View {
     var body: some View {
         VStack {
-            Text("Bem vindo a HOME PAGE")
+            headerSection
             
-            NavigationLink(destination: BuscarPassagem(), label: {
-                Text("Comprar Passagem")
-                    .foregroundColor(Color.white)
-                    .padding(10)
-                    .background{
-                        RoundedRectangle(cornerRadius: 30)
-                            .fill(Color("azulEscuro"))
-                    }
-            })
+            serviceOptions
+            
+        }
+    }
+}
+
+private extension HomePage {
+    var headerSection: some View {
+        ZStack{
+            Image("headerHome")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .overlay(Color("azulEscuro").opacity(0.4).cornerRadius(35))
+            
+            VStack{
+                Text("Aqui você encontra as\nmelhores ofertas para viajar")
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 30)
+                
+                NavigationLink(destination: BuscarPassagem(), label: {
+                    Text("Comprar Passagem")
+                        .foregroundColor(Color.white)
+                        .padding(10)
+                        .background{
+                            RoundedRectangle(cornerRadius: 30)
+                                .fill(Color("azulEscuro"))
+                        }
+                })
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .ignoresSafeArea()
+        .padding(.bottom, 30)
+    }
+    
+    var serviceOptions: some View {
+        VStack{
+            Text("O que você está procurando?")
+            
+            HStack{
+                
+            }
         }
     }
 }
