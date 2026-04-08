efbf4832d691046da497bb2462b9012764721fbb
Igor <196232533+BGNS42@users.noreply.github.com>
2026-04-08 14:34:02 -0300

    Initial Commit

---
+++ AcademyFirstChallenge/AcademyFirstChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyFirstChallengeApp.swift
+//  AcademyFirstChallenge
+//
+//  Created by IGOR TEIXEIRA CARRASCO on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AcademyFirstChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AcademyFirstChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AcademyFirstChallenge
+//
+//  Created by IGOR TEIXEIRA CARRASCO on 08/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
