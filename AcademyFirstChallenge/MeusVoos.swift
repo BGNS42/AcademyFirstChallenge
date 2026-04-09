c8409c9e333a1d015cd7ac64d049f0164b513705
Mariana Fracaroli <243894242+marifracarolis2-arch@users.noreply.github.com>
2026-04-09 14:41:07 -0300

    Criei a Header

--- AcademyFirstChallenge/MeusVoos.swift
+++ AcademyFirstChallenge/MeusVoos.swift
@@ -9,7 +9,51 @@ import SwiftUI
 
 struct MeusVoos: View {
     var body: some View {
-        Text("Bem vindo aos MEUS VOOS")
+        ZStack {
+            Color.white.ignoresSafeArea()
+            
+            VStack(spacing: 0){
+                
+                header
+               
+                
+            }
+        }
+       
+    } //var body
+    var header: some View {
+        ZStack {
+            LinearGradient (colors: [Color("gradientTop"), Color("gradientBottom")], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            HStack {
+                Button {
+                    print("Voltar") }
+                label: {
+                    Image(systemName: "arrow.left")
+                        .foregroundStyle(Color.white)
+                        .opacity(0)
+                        
+                }
+                Spacer()
+                
+                VStack(spacing: 4) {
+                    Image(systemName: "airplane")
+                        .foregroundStyle(Color.white)
+                    
+                    Text("Seus Voos")
+                        .foregroundStyle(Color.white)
+                }
+                Spacer()
+                
+                Image(systemName: "bell.fill")
+                    .foregroundColor(.white)
+            
+            }
+            .padding(.horizontal)
+            .padding(.top, 60)
+        }
+        .frame(height: 140)
     }
 }
 
