3dc18a1f780e4db5df4966128466ff7c724ac61c
Igor <196232533+BGNS42@users.noreply.github.com>
2026-04-13 15:04:01 -0300

    VoosVolta criado com base na VoosIda, leva para EscolherBagagem

--- AcademyFirstChallenge/VoosVolta.swift
+++ AcademyFirstChallenge/VoosVolta.swift
@@ -8,15 +8,97 @@
 import SwiftUI
 
 struct VoosVolta: View {
+    //@State private var vooSelecionado: String? = nil // armazena o voo selecionado
+    @State var aeroBuscaIda: String = "GRU"
+    @State var aeroBuscaVolta: String = "SDU"
+    
     var body: some View {
         VStack{
-            Text("Escolha o Voo de Volta")
-            NavigationLink(destination: EscolherBagagem(), label: {
-                Text("Escolher Volta")
-            })
+            headerSection
+            
+            
+            voosScrollView
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Gradient(colors: gradientColors))
+    }
+    
+    var headerSection: some View {
+        VStack{
+            HStack(alignment: .center, spacing: 10){
+                Image(systemName: "bell.fill")
+                    .foregroundColor(Color("offWhite"))
+                    .opacity(0)
+                
+                Spacer()
+                
+                Text(aeroBuscaVolta) // Fazer ele observar a variável escolhida na tela anterior e mostrar aqui
+                    .font(.custom("Poppins-Medium", size: 16, relativeTo: .body))
+                    .foregroundColor(Color("offWhite"))
+                Image("headerPlaneImg")
+                Text(aeroBuscaIda)
+                    .font(.custom("Poppins-Medium", size: 16, relativeTo: .body))
+                    .foregroundColor(Color("offWhite"))
+                
+                Spacer()
+                
+                Image(systemName: "bell.fill")
+                    .foregroundColor(Color("offWhite"))
+            }
+            .padding()
+            
+            HStack(alignment: .center, spacing: 10){
+                Text("Voos de ida")
+                    .font(.custom("Poppins-Medium", size: 14, relativeTo: .body))
+                    .foregroundColor(Color("offWhite"))
+                
+                Spacer()
+                
+                Image("filterIcon")
+                Text("Filtros")
+                    .font(.custom("Poppins-Medium", size: 14, relativeTo: .body))
+                    .foregroundColor(Color("offWhite"))
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+    
+    var voosScrollView: some View {
+        ScrollView{
+            NavigationLink(destination: EscolherBagagem()) {
+                voosCard(companhiaAerea: "GOL", aeroBuscaIda: $aeroBuscaVolta, aeroBuscaVolta: $aeroBuscaIda, horarioSaida: "10:20", horarioChegada: "11:20", dataSaida: "Abr 27", dataChegada: "Abr 27", codigoVoo: "CX524", valor: "640,00")
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            NavigationLink(destination: EscolherBagagem()) {
+                voosCard(companhiaAerea: "LATAM", aeroBuscaIda: $aeroBuscaVolta, aeroBuscaVolta: $aeroBuscaIda, horarioSaida: "10:20", horarioChegada: "11:20", dataSaida: "Abr 27", dataChegada: "Abr 27", codigoVoo: "SQ762", valor: "690,00")
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            NavigationLink(destination: EscolherBagagem()) {
+                voosCard(companhiaAerea: "AZUL", aeroBuscaIda: $aeroBuscaVolta, aeroBuscaVolta: $aeroBuscaIda, horarioSaida: "10:20", horarioChegada: "11:20", dataSaida: "Abr 27", dataChegada: "Abr 27", codigoVoo: "QR9866", valor: "750,00")
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            NavigationLink(destination: EscolherBagagem()) {
+                voosCard(companhiaAerea: "GOL", aeroBuscaIda: $aeroBuscaVolta, aeroBuscaVolta: $aeroBuscaIda, horarioSaida: "10:20", horarioChegada: "11:20", dataSaida: "Abr 27", dataChegada: "Abr 27", codigoVoo: "CX5244", valor: "640,00")
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            NavigationLink(destination: EscolherBagagem()) {
+                voosCard(companhiaAerea: "LATAM", aeroBuscaIda: $aeroBuscaVolta, aeroBuscaVolta: $aeroBuscaIda, horarioSaida: "10:20", horarioChegada: "11:20", dataSaida: "Abr 27", dataChegada: "Abr 27", codigoVoo: "SQ7624", valor: "690,00")
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            NavigationLink(destination: EscolherBagagem()) {
+                voosCard(companhiaAerea: "AZUL", aeroBuscaIda: $aeroBuscaVolta, aeroBuscaVolta: $aeroBuscaIda, horarioSaida: "10:20", horarioChegada: "11:20", dataSaida: "Abr 27", dataChegada: "Abr 27", codigoVoo: "QR9863", valor: "750,00")
+            }
+            .buttonStyle(PlainButtonStyle())
         }
     }
 }
+
+
 
 #Preview {
     VoosVolta()
