a7528d6396e2b14b04f28b28117914c6ab1e9869
Igor <196232533+BGNS42@users.noreply.github.com>
2026-04-14 17:45:58 -0300

    Terminado a tela de resumo do pedido

--- AcademyFirstChallenge/ResumoDoPedido.swift
+++ AcademyFirstChallenge/ResumoDoPedido.swift
@@ -14,26 +14,191 @@ struct ResumoDoPedido: View {
             headerSection(pageTitle: "Resumo do pedido", pageIcon: "")
             
             ScrollView{
-                VStack{
-                    RoundedRectangle(cornerRadius: 25)
+                ZStack(alignment: .center){
+                    RoundedRectangle(cornerRadius: 30)
                         .fill(Color.white)
-                        .clipShape(TicketCutoutShape(), style: FillStyle(eoFill: true))
+                        .clipShape(TicketCutoutShape2(), style: FillStyle(eoFill: true))
                         .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 10)
                     
-                    boraiBtn()
-                    boraiBtn()
+                    VStack{
+                        HStack{
+                            Image(.latamLogo)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: 71, maxHeight: 22)
+                                //.padding(.horizontal, 30)
+                            
+                            Spacer()
+                            
+                            Image(.boraiLogo)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: 36, maxHeight: 27)
+                                //.padding(.horizontal, 30)
+                        }
+                        
+                        Spacer(minLength: 5)
+                        
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text("GRU")
+                                    .font(.custom("Poppins-Medium", size: 20, relativeTo: .body))
+                                    .foregroundStyle(Color(.black))
+                                
+                                Text("São Paulo")
+                                    .font(.custom("Poppins-Medium", size: 12, relativeTo: .body))
+                                    .foregroundStyle(.cinzaClaro)
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            Spacer(minLength: 5)
+                            
+                            VStack(spacing: 0) {
+                                Image("airplane1")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 60)
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                            Spacer(minLength: 5)
+                            
+                            VStack(alignment: .trailing){
+                                Text("SDU")
+                                    .font(.custom("Poppins-Medium", size: 20, relativeTo: .body))
+                                    .foregroundStyle(Color(.black))
+                                
+                                Text("Rio de Janeiro")
+                                    .font(.custom("Poppins-Medium", size: 12, relativeTo: .body))
+                                    .foregroundStyle(.cinzaClaro)
+                            }
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                        .padding(.vertical, 10)
+                        
+                        Spacer(minLength: 5)
+                        
+                        VStack(alignment: .leading){
+                            Text("Ponto de Embarque: Aeroporto de Guarulhos")
+                                .font(.custom("Poppins-Medium", size: 12, relativeTo: .body))
+                                .foregroundStyle(Color(.black))
+                                .padding(.bottom, 2)
+                            
+                            Text("**Pedro Augusto Alves** - **RG**: 4398593184 - **CPF:** 12345678901")
+                                .font(.custom("Poppins-Medium", size: 11, relativeTo: .body))
+                                .foregroundStyle(Color(.cinzaClaro))
+                            
+                            Text("27/04/2026 às 13:00")
+                                .font(.custom("Poppins-Medium", size: 11, relativeTo: .body))
+                                .foregroundStyle(Color(.cinzaClaro))
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        linhaPontilhada()
+                            //.padding(.top)
+                            .padding(.horizontal, -5)
+                            //.padding(.bottom, 20)
+                        
+                        HStack{
+                            Text("Cupom de Voo")
+                                .font(.custom("Poppins-Regular", size: 11, relativeTo: .body))
+                                .foregroundStyle(.offWhite)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            Text("ADICIONAR")
+                                .font(.custom("Poppins-SemiBold", size: 9, relativeTo: .body))
+                                .foregroundStyle(.black)
+                                .frame(width: 70, alignment: .center)
+                                .padding(5)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .fill(.white)
+                                )
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 10)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(.azulEscuro)
+                        )
+                        .padding(.bottom, 30)
+                        
+                        VStack{
+                            resumoPreco()
+                            resumoPreco(title: "Desconto aplicado:", price: "R$ 0,00")
+                            resumoPreco(title: "Método de pagamento:", price: "MasterCard 0831 em 5x sem juros")
+                        }
+                        .padding(.bottom, 50)
+                        
+                        linhaPontilhada(tracejado: [100,0.1])
+                            .padding(.horizontal, -20)
+                        
+                        Text("R$ 981,55")
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .font(.custom("Poppins-Medium", size: 25, relativeTo: .body))
+                        
+                        NavigationLink(destination: PagamentoConfirmado(), label: {
+                            boraiBtn(textBtn: "Confirmar Pedido", txtColor: .offWhite, btnColor: .azulEscuro)
+                        })
+                    }
+                    .padding(20)
+                    
                 }
+                //.frame(height: 600)
             }
+            .padding(.horizontal, 20)
             
-            Text("Veja o resumo do seu pedido")
-            NavigationLink(destination: PagamentoConfirmado(), label: {
-                Text("Confirmar seu pedido")
-            })
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Gradient(colors:gradientColors))
     }
+    
+    struct TicketCutoutShape2: Shape {
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+          
+            path.addRect(rect)
+            
+        
+            let radius: CGFloat = 15
+            let midY = rect.height * 0.37
+            
+            let leftHole = CGRect(x: -radius, y: midY - radius, width: radius * 2, height: radius * 2)
+            let rightHole = CGRect(x: rect.width - radius, y: midY - radius, width: radius * 2, height: radius * 2)
+            
+            
+            path.addEllipse(in: leftHole)
+            path.addEllipse(in: rightHole)
+            return path
+            
+            
+        }
+    }
 }
+
+struct resumoPreco: View {
+    var title: String = "Total:"
+    var price: String = "R$ 981,55"
+    
+    var body: some View {
+        HStack{
+            Text(title)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                .font(.custom("Poppins-Medium", size: 12, relativeTo: .body))
+                .foregroundStyle(Color(.black))
+            
+            Text(price)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .font(.custom("Poppins-Medium", size: 12, relativeTo: .body))
+                .foregroundStyle(Color(.cinzaClaro))
+                .fixedSize(horizontal: false, vertical: true)
+                //.multilineTextAlignment(.trailing)
+        }
+    }
+}
+
 
 #Preview {
     ResumoDoPedido()
