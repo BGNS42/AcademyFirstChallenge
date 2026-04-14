1edaad233af250355980edd565168ef4d6895fce
Mariana Fracaroli <243894242+marifracarolis2-arch@users.noreply.github.com>
2026-04-14 17:58:39 -0300

    Splash view criada

---
+++ AcademyFirstChallenge/StartView.swift
@@ -0,0 +1,36 @@
+//
+//  StartView.swift
+//  AcademyFirstChallenge
+//
+//  Created by MARIANA FRACAROLI LOPES on 14/04/26.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    
+    @State private var showLogin = false
+    var body: some View {
+        
+        if !showLogin {
+            SplashView()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                        showLogin = true
+                        
+                    }
+                }
+        } else {
+            NavigationStack{
+                Login()
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    StartView()
+        .ignoresSafeArea()
+
+}
